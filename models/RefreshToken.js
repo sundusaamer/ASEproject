ca6bfe48d0bd7982e8961dc2cb4ff568f1f51378
sundusaamer <sundus.aamer2004@gmail.com>
2026-04-25 01:00:41 +0300

    fix: resolve foreign key data type conflicts and stabilize server

--- models/RefreshToken.js
+++ models/RefreshToken.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/database');
 
 const RefreshToken = sequelize.define('RefreshToken', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  userid: { type: DataTypes.BIGINT, allowNull: false },
+  userid: { type: DataTypes.INTEGER, allowNull: false },
   token: { type: DataTypes.TEXT, allowNull: false },
   expiry_date: { type: DataTypes.DATE, allowNull: false }
 }, {
--- models/ReportVote.js
+++ models/ReportVote.js
@@ -3,8 +3,8 @@ const sequelize = require('../config/database');
 
 const ReportVote = sequelize.define('ReportVote', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  report_id: { type: DataTypes.BIGINT, allowNull: false },
-  user_id: { type: DataTypes.BIGINT, allowNull: false },
+  report_id: { type: DataTypes.INTEGER, allowNull: false },
+user_id: { type: DataTypes.INTEGER, allowNull: false },
   vote_type: { 
     type: DataTypes.ENUM('upvote', 'downvote'), 
     allowNull: false 
